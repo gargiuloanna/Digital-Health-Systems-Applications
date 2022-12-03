e22b382c6a864f82527a9abb19588c9834696bcf
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2022-12-03 12:03:53 +0100

    test

---
+++ HIS_project/src/main/java/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+	private String attr;
+	
+	public Test(String attr) {
+		attr = attr;
+	}
+	
+	public String get_attr() {
+		return attr;
+	}
+	
+	public void set_attr(String attr) {
+		attr = attr;
+	}
+	
+}
