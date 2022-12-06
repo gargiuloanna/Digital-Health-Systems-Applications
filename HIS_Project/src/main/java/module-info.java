1283cfcd86cee262b0e99bdd559bb809b65b5adc
maruotto <94022256+maruotto@users.noreply.github.com>
2022-12-06 13:54:51 +0100

    Delete HIS_Project directory

--- HIS_Project/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module it.unisa.diem.dhsa.group3.HIS_Project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.dhsa.group3.HIS_Project to javafx.fxml;
-    exports it.unisa.diem.dhsa.group3.HIS_Project;
-}
