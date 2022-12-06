82331b9fcf3019063eeb781ce3906cd9ba8142e8
maruotto <94022256+maruotto@users.noreply.github.com>
2022-12-06 12:20:16 +0100

    Revert "whole project"
    This reverts commit ec707b8c7f537d5be673825358bd2db8877f70fb.

--- HIS_Project/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module it.unisa.diem.dsha.group3.HIS_Project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.dsha.group3.HIS_Project to javafx.fxml;
-    exports it.unisa.diem.dsha.group3.HIS_Project;
-}
