ec707b8c7f537d5be673825358bd2db8877f70fb
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2022-12-06 12:05:21 +0100

    whole project

---
+++ HIS_Project/src/main/java/it/unisa/diem/dsha/group3/HIS_Project/PrimaryController.java
@@ -0,0 +1,12 @@
+package it.unisa.diem.dsha.group3.HIS_Project;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ HIS_Project/src/main/java/it/unisa/diem/dsha/group3/HIS_Project/SecondaryController.java
@@ -0,0 +1,12 @@
+package it.unisa.diem.dsha.group3.HIS_Project;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
