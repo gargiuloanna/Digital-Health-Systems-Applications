03b6b86ac61885af9c5890019b17fefbc7ea7b52
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2022-12-07 12:12:40 +0100

    Added FXML file and relative controllers for the pages of the application

---
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/OpeningPageController.java
@@ -0,0 +1,30 @@
+package it.unisa.diem.dhsa.group3.HIS_Project;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+
+// Controller FINITO
+public class OpeningPageController {
+
+    
+    @FXML
+    private void patient_admission_btn()throws IOException{
+    	App.setRoot("PatientAdmission");
+    }
+    
+    @FXML
+    private void order_registration_btn ()throws IOException{
+    	App.setRoot("OrderRegistration");
+    }
+    
+    @FXML
+    private void mri_btn()throws IOException{
+    	App.setRoot("MRI");
+    } 
+
+    @FXML
+    private void patient_discharge_btn()throws IOException{
+    	App.setRoot("PatientDischarge");
+    }
+}
