00ebc352c275c3d917f0181dd10a070686d807e3
maruotto <94022256+maruotto@users.noreply.github.com>
2022-12-09 15:59:43 +0100

    Add changes when selected a pane

--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/PatientsParser.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/PatientsParser.java
@@ -1,58 +1,60 @@
 package it.unisa.diem.dhsa.group3.HIS_Project;
 
-import org.hl7.fhir.r4.model.Resource;
-import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Location;
+import org.hl7.fhir.r4.model.*;
 import org.hl7.fhir.r4.model.Location.LocationPositionComponent;
-import org.hl7.fhir.r4.model.Address;
-import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.DecimalType;
-import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.codesystems.V3MaritalStatus;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Map;
 
-
 public class PatientsParser extends CSVParser {
 
-	
 	public PatientsParser(String pathToCSV) {
 		super(pathToCSV);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public Resource createResource(String[] args,  Map<String, Integer> head) {
-		// added: id, birthdate, address, city, zip, state, deathdate, prefix, suffix, gender
+	public Resource createResource(String[] args, Map<String, Integer> head) {
+		// added: id, birthdate, address, city, zip, state, deathdate, prefix, suffix,marital, 
+		// gender, lat, lon
 		// sconosciuti: maiden
-		// marital, race, ethnicity, birthplace, county, 
+		//  race, ethnicity, birthplace, county,
 		// ssn, drivers, passport sembrano essere altri id e si possono aggiungere cosÃ¬
-		// con i dati di address e latitudine e longitudine (lat, lon) si puÃ² creare una risorsa Location
 		// hc_expenses, hc_coverage credo siano legate alla risorsa HealthcareService
 		Patient p = new Patient();
 		Location l = new Location();
+		DateFormat df = DateFormat.getDateInstance();
 		p.addIdentifier().setSystem("urn:system").setValue(args[head.get("id")]);
-		p.addName().setFamily(args[head.get("last")]).addGiven(args[head.get("first")]).addPrefix(args[head.get("prefix")]).addSuffix(args[head.get("suffix")]);
-		Address a = p.addAddress().setCity(args[head.get("city")]).addLine(args[head.get("address")]).setPostalCode(args[head.get("zip")]).setState(args[head.get("state")]);
-		p.setBirthDate(new Date(args[head.get("birthdate")]));
-		if(!args[head.get("deathdate")].isEmpty()) {
-			//p.setDeceased(new Date(args[head.get("deathdate")]));
-			//vedi metodi get
+		p.addName().setFamily(args[head.get("last")]).addGiven(args[head.get("first")])
+				.addPrefix(args[head.get("prefix")]).addSuffix(args[head.get("suffix")]);
+		Address a = p.addAddress().setCity(args[head.get("city")]).addLine(args[head.get("address")])
+				.setPostalCode(args[head.get("zip")]).setState(args[head.get("state")]);
+
+		try {
+			p.setBirthDate(df.parse(args[head.get("birthdate")]));
+			if (!args[head.get("deathdate")].isEmpty())
+				p.setDeceased(new DateType(df.parse(args[head.get("deathdate")])));
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
-		if(!args[head.get("marital")].isEmpty()) {
+
+		if (!args[head.get("marital")].isEmpty()) {
 			V3MaritalStatus status = V3MaritalStatus.valueOf(args[head.get("marital")]);
-			CodeableConcept code = new CodeableConcept(new Coding("http://terminology.hl7.org/CodeSystem/v3-MaritalStatus",status.toCode(), status.getDisplay()));
+			CodeableConcept code = new CodeableConcept(new Coding(
+					"http://terminology.hl7.org/CodeSystem/v3-MaritalStatus", status.toCode(), status.getDisplay()));
 			p.setMaritalStatus(code);
-					
+
 		}
-		l = l.setAddress(a).setPosition(new LocationPositionComponent(new DecimalType(args[head.get("lon")]), new DecimalType(args[head.get("lat")])));
-		p.setGender(Enumerations.AdministrativeGender.valueOf(args[head.get("gender")]));		
+		l = l.setAddress(a).setPosition(new LocationPositionComponent(new DecimalType(args[head.get("lon")]),
+				new DecimalType(args[head.get("lat")])));
+		p.setGender(Enumerations.AdministrativeGender.valueOf(args[head.get("gender")]));
 		
-		
+
 		return p;
 	}
+
 
 }
