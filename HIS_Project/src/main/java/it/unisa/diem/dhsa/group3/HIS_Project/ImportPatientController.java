af3a82b30acad6619ed6f68c56a112ea56667624
maruotto <94022256+maruotto@users.noreply.github.com>
2022-12-15 18:39:45 +0100

    Add printing in textfield

--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/ImportPatientController.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/ImportPatientController.java
@@ -9,56 +9,100 @@ import org.hl7.fhir.r4.model.Resource;
 
 import it.unisa.diem.dhsa.group3.CSV.ReadCSV;
 import it.unisa.diem.dhsa.group3.resources.PatientResource;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
-
 
 // Controller FINITO
 public class ImportPatientController {
 
-    
-    @FXML
-    private TextField patientField;
+	@FXML
+	private TextField allergyField;
 
-    @FXML
-    private TextField patientField1;
+	@FXML
+	private TextField careplansField;
 
-    @FXML
-    private TextField patientField2;
+	@FXML
+	private TextField conditionField;
 
-    @FXML
-    private TextField patientField3;
+	@FXML
+	private TextField devicesField;
 
-    @FXML
-    private TextField patientField4;
+	@FXML
+	private TextField encounterField;
 
-    @FXML
-    private TextField patientField5;
+	@FXML
+	private TextField imagingStudiesField;
 
-    @FXML
-    void SwitchToOpeningPage() throws IOException {
-    	App.setRoot("OpeningPage");
-    }
+	@FXML
+	private TextField immunizationField;
 
-    @FXML
-    private void load_csv_btn()throws IOException{
-    	FileChooser chooser = new FileChooser();
-    	File filename = chooser.showOpenDialog(null);
-    	if (filename != null) {
-    		// System.out.println(filename);
-    		try {
-    			Map<String, Resource> patients = ReadCSV.readCSV(PatientResource.class, filename.getCanonicalPath());
+	@FXML
+	private TextField medicationField;
 
-    		} catch (IOException e) {
-    			System.out.println(e.getStackTrace());
-    		}
+	@FXML
+	private TextField observationField;
 
-    	}
-    }
-    
-    
-    
+	@FXML
+	private TextField organizationField;
+
+	@FXML
+	private TextField patientField;
+
+	@FXML
+	private TextField payerField;
+
+	@FXML
+	private TextField proceduresField;
+
+	@FXML
+	private TextField providerField;
+
+	@FXML
+	private TextField supplyField;
+
+	@FXML
+	private TextField transactionField;
+
+	@FXML
+	void send_clicked(ActionEvent event) {
+		
+	}
+
+	@FXML
+	void SwitchToOpeningPage() throws IOException {
+		App.setRoot("OpeningPage");
+	}
+
+	@FXML
+	private void load_csv_btn(ActionEvent event) throws IOException {
+		
+		Button source = (Button) event.getSource();
+		HBox hbox = (HBox) source.getParent();
+		TextField tf = (TextField) hbox.getChildren().get(1);
+		try {
+			String filename = file_chooser();
+			tf.setText(filename);
+			//Map<String, Resource> patients = ReadCSV.readCSV(PatientResource.class, filename);
+
+		} catch (IOException | NullPointerException e) {
+			System.out.println(e.getStackTrace());
+		}
+
+	}
+
+	private String file_chooser() throws IOException {
+		FileChooser chooser = new FileChooser();
+		File filename = chooser.showOpenDialog(null);
+		if (filename != null) {
+			// System.out.println(filename);
+			return filename.getCanonicalPath();
+		}
+		throw new NullPointerException("file not selected");
+	}
+
 }
-
