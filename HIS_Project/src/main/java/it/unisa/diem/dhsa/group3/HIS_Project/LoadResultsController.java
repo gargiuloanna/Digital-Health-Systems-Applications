00ef3f27b1438540ba2ec9545df635b52e38a4a7
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2023-01-15 18:12:36 +0100

    Controllers

---
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/LoadResultsController.java
@@ -0,0 +1,211 @@
+package it.unisa.diem.dhsa.group3.HIS_Project;
+
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.ImagingStudy;
+import org.hl7.fhir.r4.model.ServiceRequest;
+
+import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import it.unisa.diem.dhsa.group3.resources.DiagnosticReportResource;
+import it.unisa.diem.dhsa.group3.resources.ImagingStudyResource;
+import it.unisa.diem.dhsa.group3.state.PDF;
+import it.unisa.diem.dhsa.group3.state.ServerInteraction;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+public class LoadResultsController extends BasicController{
+
+	@FXML
+    private TextField bodycodeField;
+    @FXML
+    private TextField bodydesField;
+    @FXML
+    private TextArea conclusionField;
+    @FXML
+    private Button confirmButton;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TextArea detailsField;
+    @FXML
+    private TextField encounterField;
+    @FXML
+    private Button lodButton;
+    @FXML
+    private TextField modcodeField;
+    @FXML
+    private TextField moddesField;
+    @FXML
+    private TextField patientField;
+    @FXML
+    private TextField requestField;
+    @FXML
+    private TextField sopcodeField;
+    @FXML
+    private TextField sopdesField;
+
+    
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		if(MRIController.selectedlist.size() == 0) {
+    		Alert alert = new Alert(AlertType.INFORMATION, "Select an order", ButtonType.OK);
+			alert.showAndWait();
+    	}
+    	else {
+    	patientField.setText(MRIController.selectedlist.get(0).getSubject().getIdentifier().getValue());
+    	requestField.setText(MRIController.selectedlist.get(0).getId());
+    	Date date = MRIController.selectedlist.get(0).getDate();
+    	datePicker.setValue(LocalDate.of(date.getYear() + 1900, date.getMonth()+1, date.getDate()));
+    	encounterField.setText(MRIController.selectedlist.get(0).getEncounter().getIdentifier().getValue());
+    	}
+	}
+	
+	@FXML
+    void confirmAction(ActionEvent event) {
+		DiagnosticReportResource r = createDiagnosticReport();
+    	try {
+    		PDF.createPDF(r.getPatientId(), 
+    				r.getEncounter().toString(), 
+    				r.getConclusion(), 
+    				r.getServiceRequest().getNoteFirstRep().getText(), 
+    				r.getId(), 
+    				r.getServiceRequest().getIdentifierFirstRep().toString(), 
+    				r.getImagingStudy().getIdentifierFirstRep().toString());
+    		upload((DiagnosticReport) r.createResource());
+			//PDF.createPDF("encounter","patientId","conclusion", "nodetails", "id");
+			Alert alert = new Alert(AlertType.CONFIRMATION, "Results Loaded",ButtonType.OK);
+			alert.showAndWait();
+			handle(event, "MRI");
+		} catch (IOException e) {
+			Alert alert = new Alert(AlertType.ERROR, "Error in the creation of the PDF\n Please Retry.",ButtonType.OK);
+			alert.showAndWait();
+		}
+    }
+
+    @FXML
+    void loadAction(ActionEvent event) {
+    	//if we want to load the image
+    }    
+	
+	private DiagnosticReportResource createDiagnosticReport() {
+    	
+    	if(emptyFields()) {
+    		Alert alert = new Alert(AlertType.ERROR, "Fill the fields",ButtonType.OK);
+			alert.showAndWait();
+			
+			return null;
+    	}
+    	else {
+    	DiagnosticReportResource r = new DiagnosticReportResource();
+    	ImagingStudyResource i = new ImagingStudyResource(
+    			Date.from(datePicker.getValue().atStartOfDay(ZoneId.of("Asia/Kolkata")).toInstant()), patientField.getText(), encounterField.getText(), 
+    			bodycodeField.getText(), bodydesField.getText(), modcodeField.getText(), moddesField.getText(),
+    			sopcodeField.getText(), sopdesField.getText());
+    	
+    	r.setPatientId(patientField.getText());
+    	//set patient not done
+    	//set encounter not done
+    	r.setServiceRequest((ServiceRequest) MRIController.selectedlist.get(0).createResource());
+    	r.setImagingStudy((ImagingStudy) i.createResource());
+    	r.setConclusion(conclusionField.getText());
+    	
+    	return r;
+    	}
+    	
+    	
+
+	}
+	
+	private void handle(ActionEvent event, String fxml) {
+    	Stage stage;
+        Parent root;
+        stage=(Stage) ((Button)(event.getSource())).getScene().getWindow();
+        try {
+			root = FXMLLoader.load(App.class.getResource(fxml + ".fxml"));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+	
+	private void upload(DiagnosticReport r){
+		Service<String> upload = new Service<String>() {
+
+			@Override
+			protected Task<String> createTask() throws FhirClientConnectionException {
+				
+				return new Task<String>() {
+
+					@Override
+					protected String call() throws FhirClientConnectionException {
+						return ServerInteraction.uploadResource(r.getIdentifierFirstRep().getValue(), r, true);
+
+					};
+				};
+			}
+		};
+
+		upload.start();
+		upload.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+
+			@Override
+			public void handle(WorkerStateEvent event) {
+				String id = upload.getValue();
+				Alert alert = new Alert(AlertType.NONE, "Request with id:" +id +" updated correctly.",
+						ButtonType.OK);
+				alert.showAndWait();
+			}
+		});
+		
+		upload.setOnFailed(new EventHandler<WorkerStateEvent>() {
+
+			@Override
+			public void handle(WorkerStateEvent event) {
+				if (upload.getException() != null
+						&& upload.getException().getClass() == FhirClientConnectionException.class) {
+					Alert alert = new Alert(AlertType.ERROR, "Error in the connection to the server.\nPlease retry.",
+							ButtonType.OK);
+					alert.showAndWait();
+				}
+
+			}
+		});
+	}
+	
+	private boolean emptyFields() {
+		if (bodycodeField.getText().isBlank() || bodycodeField.getText().isEmpty() ||
+ 	   bodydesField.getText().isBlank() || bodydesField.getText().isEmpty() ||
+ 	   modcodeField.getText().isBlank() || modcodeField.getText().isEmpty() ||
+ 	   moddesField.getText().isBlank() || moddesField.getText().isEmpty() ||
+ 	   sopcodeField.getText().isBlank() || sopcodeField.getText().isEmpty() ||
+ 	   sopdesField.getText().isBlank() || sopdesField.getText().isEmpty())
+			return true;
+		return false;
+	}
+	}
--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/OrderRegistrationController.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/HIS_Project/OrderRegistrationController.java
@@ -1,26 +1,35 @@
 package it.unisa.diem.dhsa.group3.HIS_Project;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.beans.binding.Bindings;
+import java.time.ZoneId;
+
+import java.util.Date;
+
+import org.hl7.fhir.r4.model.Encounter;
+import org.hl7.fhir.r4.model.Identifier;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.ServiceRequest;
+
+import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
+import it.unisa.diem.dhsa.group3.resources.ServiceRequestResource;
+import it.unisa.diem.dhsa.group3.state.ServerInteraction;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Alert.AlertType;
 
 public class OrderRegistrationController extends BasicController{
-
-    @FXML
-    private TextField CodeField;
-
-    @FXML
-    private TextArea DetailsField;
-
-    @FXML
-    private TextField Field;
 
     @FXML
     private TextField IDField;
@@ -29,49 +38,145 @@ public class OrderRegistrationController extends BasicController{
     private Button SendOrderButton;
 
     @FXML
-    private TextField StatsField;
+    private MenuButton category;
 
     @FXML
-    private TextField SubjectField;
+    private DatePicker datepicker;
 
     @FXML
-    private TextField TargetField;
+    private TextArea details;
 
     @FXML
-    private TextField searchPatientField;
+    private TextField encounterField;
+
+    @FXML
+    private MenuButton intent;
+
+    @FXML
+    private TextField patientField;
+
+    @FXML
+    private TextField requestField;
+
+    @FXML
+    private TextField requesterField;
+
+    @FXML
+    private MenuButton status;
 
     @FXML
     private CheckBox summaryButton;
-    
-    @FXML
-    private VBox VboxContainer;
-    
-    @Override
-	public void initialize(URL url, ResourceBundle rb) {
-
-    	VboxContainer.setDisable(true);
-    	SendOrderButton.disableProperty().bind(Bindings.isEmpty(searchPatientField.textProperty())
-    			.and(Bindings.isEmpty(IDField.textProperty())));
-
-	}
 
     @FXML
-    void searchCode(ActionEvent event) {
-		VboxContainer.setDisable(false);
-		searchPatientField.setDisable(true);
-		searchPatientField.clear();
+    void categorySelected(ActionEvent event) {
+    	MenuItem e = (MenuItem) event.getSource();
+		category.setText(e.getText());
     }
 
     @FXML
+    void intentSelected(ActionEvent event) {
+    	MenuItem e = (MenuItem) event.getSource();
+		intent.setText(e.getText());
+    }
+
+    @FXML
+    void statusSelected(ActionEvent event) {
+    	MenuItem e = (MenuItem) event.getSource();
+		status.setText(e.getText());
+    }
+
+    
+    @FXML
     void sendOrderPressed(ActionEvent event) {
-    	if (VboxContainer.isDisable()) {
-    		searchCode(event);
-    	}
+    	ServiceRequestResource r = create(event);
+    	upload((ServiceRequest) r.createResource());
     }
 
     @FXML
     void summaryChecked(ActionEvent event) {
-
+    	//implements a pdf?
+    }
+    
+    
+    private ServiceRequestResource create(ActionEvent event) {
+    	if(emptyFields()) {
+    		Alert alert = new Alert(AlertType.ERROR, "Fill all the fields",
+					ButtonType.OK);
+			alert.showAndWait();
+    	}else {
+    	Patient p = (Patient) ServerInteraction.getResource(patientField.getText());
+    	Encounter e = new Encounter();
+    	e.addIdentifier(new Identifier().setSystem("https://github.com/synthetichealth/synthea").setValue(encounterField.getText()));
+    	ServiceRequestResource r = new ServiceRequestResource(IDField.getText(), status.getText(), intent.getText(), requestField.getText(), 
+    			p, e, category.getText(), 
+    			Date.from(datepicker.getValue().atStartOfDay(ZoneId.of("Asia/Kolkata")).toInstant()), new Date(), requesterField.getText(), details.getText());
+		return r;
+    	}
+    	
+    	return null;
+		
     }
 
+    private void upload(ServiceRequest r) {
+    	Service<String> upload = new Service<String>() {
+
+			@Override
+			protected Task<String> createTask() throws FhirClientConnectionException {
+				
+				return new Task<String>() {
+
+					@Override
+					protected String call() throws FhirClientConnectionException {
+						return ServerInteraction.uploadResource(r.getIdentifierFirstRep().getValue(), r, true);
+
+					};
+				};
+			}
+		};
+
+		upload.start();
+		upload.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+
+			@Override
+			public void handle(WorkerStateEvent event) {
+				String id = upload.getValue();
+				Alert alert = new Alert(AlertType.NONE, "Request with id:" +id +" updated correctly.",
+						ButtonType.OK);
+				alert.showAndWait();
+			}
+		});
+		
+		upload.setOnFailed(new EventHandler<WorkerStateEvent>() {
+
+			@Override
+			public void handle(WorkerStateEvent event) {
+				if (upload.getException() != null
+						&& upload.getException().getClass() == FhirClientConnectionException.class) {
+					Alert alert = new Alert(AlertType.ERROR, "Error in the connection to the server.\nPlease retry.",
+							ButtonType.OK);
+					alert.showAndWait();
+				}
+
+			}
+		});
+    }
+    
+    
+    private boolean emptyFields() {
+    	if(IDField.getText().isBlank() || IDField.getText().isEmpty() ||
+    	   status.getText().isBlank() || status.getText().isEmpty() ||
+    	   intent.getText().isBlank() || intent.getText().isEmpty() ||
+    	   requestField.getText().isBlank() || requestField.getText().isEmpty() || 
+    	   patientField.getText().isBlank() || patientField.getText().isEmpty() ||
+    	   encounterField.getText().isBlank() || encounterField.getText().isEmpty() ||
+    	   category.getText().isBlank() || category.getText().isEmpty() || 
+    	   datepicker.getValue().toString() == "" || datepicker.getValue().toString() == null ||
+    	   category.getText().isBlank() || category.getText().isEmpty() || 
+    	   requesterField.getText().isBlank() || requesterField.getText().isEmpty() ||
+    	   details.getText().isBlank() || details.getText().isEmpty())
+    		return true;
+    
+    return false;
+    }
+    
 }
