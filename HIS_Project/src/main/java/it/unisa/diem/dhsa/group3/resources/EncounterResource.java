9d5ed64a8c1ead59cc484cd8e10f2f21ab09fe78
Luigina25 <93980362+Luigina25@users.noreply.github.com>
2023-01-18 15:52:38 +0100

    update EncounterResource and MedicationResource

--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/resources/EncounterResource.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/resources/EncounterResource.java
@@ -1,7 +1,6 @@
 package it.unisa.diem.dhsa.group3.resources;
 
-//import java.util.Date;
-import java.sql.Date;
+import java.util.Date;
 
 import org.hl7.fhir.r4.model.*;
 
@@ -12,197 +11,164 @@ import it.unisa.diem.dhsa.group3.enumerations.EncounterClass;
 import it.unisa.diem.dhsa.group3.enumerations.EncountersSettingCode;
 import it.unisa.diem.dhsa.group3.state.Memory;
 
-public class EncounterResource extends BaseResource{
-	
-	//Id,START,STOP,PATIENT,ORGANIZATION,PROVIDER,PAYER,ENCOUNTERCLASS,CODE,DESCRIPTION,BASE_ENCOUNTER_COST,TOTAL_CLAIM_COST,
-	//PAYER_COVERAGE,REASONCODE,REASONDESCRIPTION
-	
+public class EncounterResource extends BaseResource {
 
 	@CsvBindByName
 	@CsvDate("yyyy-MM-dd")
 	private Date START;
-	
+
 	@CsvBindByName
 	@CsvDate("yyyy-MM-dd")
 	private Date STOP;
-	
+
 	@CsvBindByName
 	private String PATIENT = "";
-	
+
 	@CsvBindByName
 	private String ORGANIZATION = "";
-	
+
 	@CsvBindByName
 	private String PROVIDER = "";
-	
+
 	@CsvBindByName
 	private String PAYER = "";
-	
+
 	@CsvBindByName
 	private String ENCOUNTERCLASS = "";
-	
+
 	@CsvBindByName
 	private String CODE;
-	
+
 	@CsvBindByName
 	private String DESCRIPTION = "";
-	
+
 	@CsvBindByName
 	private Float BASE_ENCOUNTER_COST;
-	
+
 	@CsvBindByName
 	private Float TOTAL_CLAIM_COST;
-	
+
 	@CsvBindByName
 	private Float PAYER_COVERAGE;
-	
+
 	@CsvBindByName
-	private String REASONCODE; 
-	
+	private String REASONCODE;
+
 	@CsvBindByName
 	private String REASONDESCRIPTION = "";
-	
 
 	public Date getSTART() {
 		return START;
 	}
 
-
 	public void setSTART(Date sTART) {
 		START = sTART;
 	}
-
 
 	public Date getSTOP() {
 		return STOP;
 	}
 
-
 	public void setSTOP(Date sTOP) {
 		STOP = sTOP;
 	}
-
 
 	public String getPATIENT() {
 		return PATIENT;
 	}
 
-
 	public void setPATIENT(String pATIENT) {
 		PATIENT = pATIENT;
 	}
-
 
 	public String getORGANIZATION() {
 		return ORGANIZATION;
 	}
 
-
 	public void setORGANIZATION(String oRGANIZATION) {
 		ORGANIZATION = oRGANIZATION;
 	}
-
 
 	public String getPROVIDER() {
 		return PROVIDER;
 	}
 
-
 	public void setPROVIDER(String pROVIDER) {
 		PROVIDER = pROVIDER;
 	}
-
 
 	public String getPAYER() {
 		return PAYER;
 	}
 
-
 	public void setPAYER(String pAYER) {
 		PAYER = pAYER;
 	}
-
 
 	public String getENCOUNTERCLASS() {
 		return ENCOUNTERCLASS;
 	}
 
-
 	public void setENCOUNTERCLASS(String eNCOUNTERCLASS) {
 		ENCOUNTERCLASS = eNCOUNTERCLASS;
 	}
-
 
 	public String getCODE() {
 		return CODE;
 	}
 
-
 	public void setCODE(String cODE) {
 		CODE = cODE;
 	}
-
 
 	public String getDESCRIPTION() {
 		return DESCRIPTION;
 	}
 
-
 	public void setDESCRIPTION(String dESCRIPTION) {
 		DESCRIPTION = dESCRIPTION;
 	}
-
 
 	public Float getBASE_ENCOUNTER_COST() {
 		return BASE_ENCOUNTER_COST;
 	}
 
-
 	public void setBASE_ENCOUNTER_COST(Float bASE_ENCOUNTER_COST) {
 		BASE_ENCOUNTER_COST = bASE_ENCOUNTER_COST;
 	}
-
 
 	public Float getTOTAL_CLAIM_COST() {
 		return TOTAL_CLAIM_COST;
 	}
 
-
 	public void setTOTAL_CLAIM_COST(Float tOTAL_CLAIM_COST) {
 		TOTAL_CLAIM_COST = tOTAL_CLAIM_COST;
 	}
-
 
 	public Float getPAYER_COVERAGE() {
 		return PAYER_COVERAGE;
 	}
 
-
 	public void setPAYER_COVERAGE(Float pAYER_COVERAGE) {
 		PAYER_COVERAGE = pAYER_COVERAGE;
 	}
-
 
 	public String getREASONCODE() {
 		return REASONCODE;
 	}
 
-
 	public void setREASONCODE(String rEASONCODE) {
 		REASONCODE = rEASONCODE;
 	}
-
 
 	public String getREASONDESCRIPTION() {
 		return REASONDESCRIPTION;
 	}
 
-
 	public void setREASONDESCRIPTION(String rEASONDESCRIPTION) {
 		REASONDESCRIPTION = rEASONDESCRIPTION;
 	}
 
-	
 	@Override
 	public String toString() {
 		return "EncounterResource [Id=" + super.getId() + ", START=" + START + ", STOP=" + STOP + ", PATIENT=" + PATIENT
@@ -211,87 +177,79 @@ public class EncounterResource extends BaseResource{
 				+ BASE_ENCOUNTER_COST + ", TOTAL_CLAIM_COST=" + TOTAL_CLAIM_COST + ", PAYER_COVERAGE=" + PAYER_COVERAGE
 				+ ", REASONCODE=" + REASONCODE + ", REASONDESCRIPTION=" + REASONDESCRIPTION + "]";
 	}
-
-
-	public Resource createResource() {
-
-		Encounter e = new Encounter() ;
-		// Definition of the considered profile
-		e.setMeta(new Meta().addProfile("http://hl7.org/fhir/us/core/StructureDefinition/us-core-encounter"));
-		
-		//set identifier
-		e.addIdentifier().setSystem("https://github.com/synthetichealth/synthea").setValue(super.getId());
-		
-		//set period
-		Period period = new Period();
-		if(STOP != null)		
-			period.setStart(START).setEnd(STOP);
-		else
-			period.setStart(START);
-		e.setPeriod(period);
-		
-		e.setStatus(getStatus());
-		//set patient
-		Patient patient = (Patient) Memory.getMemory().get(PatientResource.class).get(PATIENT); //get the patient with id PATIENT 
-		e.setSubjectTarget(patient);
-		
-		//set organization
-		Organization o = (Organization) Memory.getMemory().get(OrganizationResource.class).get(ORGANIZATION);
-		e.setServiceProviderTarget(o);
-		
-		
-		//set provider
-		PractitionerRole practitioner = (PractitionerRole) Memory.getMemory().get(ProviderResource.class).get(PROVIDER);
-		e.addParticipant().setIndividualTarget(practitioner);
-		
-		//set payer
-		Organization payer = (Organization) Memory.getMemory().get(PayerResource.class).get(PAYER);
-		Account a = new Account();
-		a.addCoverage().setCoverage(new Reference().setIdentifier(payer.getIdentifier().get(0)));
-
-		//class --TODO: controllare la classe, prof disse: fare con SNOMED regimeTherapy  o con snomed o con us core 
-		EncounterClass eclass = EncounterClass.fromCSV(ENCOUNTERCLASS);
-		e.setClass_(new Coding(eclass.getSystem(), eclass.toCode(), eclass.getDefinition()));
-		
-		//code & description
-		EncountersSettingCode code = EncountersSettingCode.fromCSV(DESCRIPTION);
-		e.addType(new CodeableConcept(new Coding(code.getSystem(), code.toCode(), code.getDefinition())));
-		
-		//TODO:base encounter cost e total claim cost
-		//Annotation ann = new Annotation();
-		//ann.setText("TOTAL_CLAIM_COST= " + TOTAL_CLAIM_COST +"PAYER_COVERAGE= "+PAYER_COVERAGE);
-		//e.addNote(ann);
-		
-		//add reason code -- TODO: check SNOMED codes!!!
-		e.addReasonCode(new CodeableConcept(new Coding("https://www.snomed.org/", REASONCODE, REASONDESCRIPTION)));
-		
-		
-		return e;
-		
-		
-	}
-
+	
 	private Encounter.EncounterStatus getStatus() {
 		if (STOP.after(START) && STOP != null)
 			return Encounter.EncounterStatus.FINISHED;
-		if(START != null && STOP == null)
+		if (START != null && STOP == null)
 			return Encounter.EncounterStatus.INPROGRESS;
-		if(START == null && STOP == null)
+		if (START == null && STOP == null)
 			return Encounter.EncounterStatus.NULL;
-		if(START.after(DateTimeType.now().getValue()))
+		if (START.after(DateTimeType.now().getValue()))
 			return Encounter.EncounterStatus.PLANNED;
 		if (STOP.before(START))
 			return Encounter.EncounterStatus.CANCELLED;
 		else
 			return Encounter.EncounterStatus.UNKNOWN;
-	
+
 	}
-	
-	
-	
-	
-	
-	
+
+	public Resource createResource() {
+
+		Encounter e = new Encounter();
+		// Definition of the considered profile
+		e.setMeta(new Meta().addProfile("http://hl7.org/fhir/us/core/StructureDefinition/us-core-encounter"));
+
+		// set identifier
+		e.addIdentifier().setSystem("https://github.com/synthetichealth/synthea").setValue(super.getId());
+
+		// set period
+		Period period = new Period();
+		if (STOP != null)
+			period.setStart(START).setEnd(STOP);
+		else
+			period.setStart(START);
+		e.setPeriod(period);
+
+		e.setStatus(getStatus());
+		
+		// set patient
+		Patient patient = (Patient) Memory.getMemory().get(PatientResource.class).get(PATIENT); 
+		e.setSubject(new Reference(patient));
+
+		// set organization
+		Organization o = (Organization) Memory.getMemory().get(OrganizationResource.class).get(ORGANIZATION);
+		e.setServiceProvider(new Reference(o));
+
+		// set provider
+		Practitioner practitioner = (Practitioner) Memory.getMemory().get(ProviderResource.class).get(PROVIDER);
+		e.addParticipant().setIndividual(new Reference(practitioner));
+
+		// set payer
+		/*Organization payer = (Organization) Memory.getMemory().get(PayerResource.class).get(PAYER);
+		Account a = new Account();
+		a.addCoverage().setCoverage(new Reference().setIdentifier(payer.getIdentifier().get(0)));*/
+
+
+		EncounterClass eclass = EncounterClass.fromCSV(ENCOUNTERCLASS);
+		e.setClass_(new Coding(eclass.getSystem(), eclass.toCode(), eclass.getDefinition()));
+
+		// code & description
+		//EncountersSettingCode code = EncountersSettingCode.fromCSV(DESCRIPTION);
+		e.addType(new CodeableConcept(new Coding("https://www.snomed.org/",CODE, DESCRIPTION)));
+
+		// TODO:base encounter cost e total claim cost
+		// Annotation ann = new Annotation();
+		// ann.setText("TOTAL_CLAIM_COST= " + TOTAL_CLAIM_COST +"PAYER_COVERAGE=
+		// "+PAYER_COVERAGE);
+		// e.addNote(ann);
+
+		// add reason code 
+		e.addReasonCode(new CodeableConcept(new Coding("https://www.snomed.org/", REASONCODE, REASONDESCRIPTION)));
+
+		return e;
+
+	}
+
 
 }
-
--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/resources/MedicationResource.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/resources/MedicationResource.java
@@ -1,6 +1,6 @@
 package it.unisa.diem.dhsa.group3.resources;
 
-import java.sql.Date;
+import java.util.Date;
 
 import org.hl7.fhir.r4.model.Annotation;
 import org.hl7.fhir.r4.model.CodeableConcept;
@@ -12,6 +12,7 @@ import org.hl7.fhir.r4.model.MedicationStatement;
 import org.hl7.fhir.r4.model.Money;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Period;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
 
 import com.opencsv.bean.CsvBindByName;
@@ -19,208 +20,153 @@ import com.opencsv.bean.CsvDate;
 
 import it.unisa.diem.dhsa.group3.state.Memory;
 
-public class MedicationResource extends BaseResource{
-	
+public class MedicationResource extends BaseResource {
 
 	@CsvBindByName
-	@CsvDate("yyyy-MM-dd'T'HH:mm'Z'")
+	@CsvDate("yyyy-MM-dd'T'HH:mm")
 	private Date START;
-	
+
 	@CsvBindByName
-	@CsvDate("yyyy-MM-dd'T'HH:mm'Z'")
+	@CsvDate("yyyy-MM-dd'T'HH:mm")
 	private Date STOP;
-	
+
 	@CsvBindByName
 	private String PATIENT = "";
-	
+
 	@CsvBindByName
 	private String PAYER = "";
-	
+
 	@CsvBindByName
 	private String ENCOUNTER = "";
-	
+
 	@CsvBindByName
 	private String CODE;
-	
+
 	@CsvBindByName
 	private String DESCRIPTION = "";
-	
+
 	@CsvBindByName
 	private Float BASE_COST;
 
 	@CsvBindByName
 	private Float PAYER_COVERAGE;
-	
+
 	@CsvBindByName
 	private int DISPENSES;
-	
+
 	@CsvBindByName
 	private Float TOTALCOST;
-	
+
 	@CsvBindByName
-	private String REASONCODE; //o è un numero o è null
-	
+	private String REASONCODE; // o è un numero o è null
+
 	@CsvBindByName
 	private String REASONDESCRIPTION = "";
-	
-	
-	
+
 	public Date getSTART() {
 		return START;
 	}
-
-
 
 	public void setSTART(Date sTART) {
 		START = sTART;
 	}
 
-
-
 	public Date getSTOP() {
 		return STOP;
 	}
-
-
 
 	public void setSTOP(Date sTOP) {
 		STOP = sTOP;
 	}
 
-
-
 	public String getPATIENT() {
 		return PATIENT;
 	}
-
-
 
 	public void setPATIENT(String pATIENT) {
 		PATIENT = pATIENT;
 	}
 
-
-
 	public String getPAYER() {
 		return PAYER;
 	}
-
-
 
 	public void setPAYER(String pAYER) {
 		PAYER = pAYER;
 	}
 
-
-
 	public String getENCOUNTER() {
 		return ENCOUNTER;
 	}
-
-
 
 	public void setENCOUNTER(String eNCOUNTER) {
 		ENCOUNTER = eNCOUNTER;
 	}
 
-
-
 	public String getCODE() {
 		return CODE;
 	}
-
-
 
 	public void setCODE(String cODE) {
 		CODE = cODE;
 	}
 
-
-
 	public String getDESCRIPTION() {
 		return DESCRIPTION;
 	}
-
-
 
 	public void setDESCRIPTION(String dESCRIPTION) {
 		DESCRIPTION = dESCRIPTION;
 	}
 
-
-
 	public Float getBASE_COST() {
 		return BASE_COST;
 	}
-
-
 
 	public void setBASE_COST(Float bASE_COST) {
 		BASE_COST = bASE_COST;
 	}
 
-
-
 	public Float getPAYER_COVERAGE() {
 		return PAYER_COVERAGE;
 	}
-
-
 
 	public void setPAYER_COVERAGE(Float pAYER_COVERAGE) {
 		PAYER_COVERAGE = pAYER_COVERAGE;
 	}
 
-
-
 	public int getDISPENSES() {
 		return DISPENSES;
 	}
-
-
 
 	public void setDISPENSES(int dISPENSES) {
 		DISPENSES = dISPENSES;
 	}
 
-
-
 	public Float getTOTALCOST() {
 		return TOTALCOST;
 	}
-
-
 
 	public void setTOTALCOST(Float tOTALCOST) {
 		TOTALCOST = tOTALCOST;
 	}
 
-
-
 	public String getREASONCODE() {
 		return REASONCODE;
 	}
-
-
 
 	public void setREASONCODE(String rEASONCODE) {
 		REASONCODE = rEASONCODE;
 	}
 
-
-
 	public String getREASONDESCRIPTION() {
 		return REASONDESCRIPTION;
 	}
-
-
 
 	public void setREASONDESCRIPTION(String rEASONDESCRIPTION) {
 		REASONDESCRIPTION = rEASONDESCRIPTION;
 	}
 
-
-	
 	@Override
 	public String toString() {
 		return "MedicationResource [START=" + START + ", STOP=" + STOP + ", PATIENT=" + PATIENT + ", PAYER=" + PAYER
@@ -230,45 +176,42 @@ public class MedicationResource extends BaseResource{
 	}
 
 
-
-	@SuppressWarnings("deprecation")
 	@Override
 	public Resource createResource() {
 		MedicationStatement med = new MedicationStatement();
-		
-		//add effective period: the period in which the medication was/is taken
-		if(STOP != null)
+
+		// add effective period: the period in which the medication was/is taken
+		if (STOP != null)
 			med.setEffective(new Period().setEnd(STOP).setStart(START));
 		else
 			med.setEffective(new Period().setStart(START));
-		
-		//add patient
-		Patient p = (Patient) Memory.getMemory().get(OrganizationResource.class).get(PATIENT);
-		med.setSubjectTarget(p);
-		
-		//add payer TODO: payer
-		//Resource payer = Memory.getMemory().get(PayerResource.class).get(PAYER);
-		
-		
-		//add encounter
-		Encounter e = (Encounter) Memory.getMemory().get(OrganizationResource.class).get(ENCOUNTER);
-		med.setContextTarget(e);
-		
-		//add base cost for the medicine
+
+		// add patient
+		Patient p = (Patient) Memory.getMemory().get(PatientResource.class).get(PATIENT);
+		med.setSubject(new Reference(p));
+
+		// add encounter
+		Encounter e = (Encounter) Memory.getMemory().get(EncounterResource.class).get(ENCOUNTER);
+		med.setContext(new Reference(e));
+
+		// add base cost for the medicine
 		MedicationKnowledge cost = new MedicationKnowledge();
-		cost.addCost(new MedicationKnowledge.MedicationKnowledgeCostComponent().setCost(new Money().setCurrency("USD").setValue(BASE_COST)));
-		cost.getAssociatedMedicationTarget().add(new Medication().setCode(new CodeableConcept(new Coding("www.nlm.nih.gov/research/umls/rxnorm", CODE, DESCRIPTION))));
-		
-		//set medication --SYSTEM: RxNORM
+		cost.addCost(new MedicationKnowledge.MedicationKnowledgeCostComponent()
+				.setCost(new Money().setCurrency("USD").setValue(BASE_COST)));
+		cost.getAssociatedMedication().add(new Reference(new Medication()
+				.setCode(new CodeableConcept(new Coding("www.nlm.nih.gov/research/umls/rxnorm", CODE, DESCRIPTION)))));
+
+		// set medication --SYSTEM: RxNORM
 		med.setMedication(new CodeableConcept(new Coding("www.nlm.nih.gov/research/umls/rxnorm", CODE, DESCRIPTION)));
-		
-		//add reason
+
+		// add reason
 		med.addReasonCode(new CodeableConcept(new Coding("https://www.snomed.org/", REASONCODE, REASONDESCRIPTION)));
+
+		// add expenses
+		/*Annotation a = new Annotation();
+		a.setText("DISPENSES= " + DISPENSES + "TOTALCOST= " + TOTALCOST + "PAYER_COVERAGE= " + PAYER_COVERAGE);
+		med.addNote(a);*/
 		
-		//add expenses
-		Annotation a = new Annotation();
-		a.setText("DISPENSES= " + DISPENSES + "TOTALCOST= "+TOTALCOST+"PAYER_COVERAGE= "+PAYER_COVERAGE);
-		med.addNote(a);
 		return med;
 	}
 
