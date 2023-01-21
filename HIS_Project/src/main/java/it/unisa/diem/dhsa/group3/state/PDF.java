92bc661cbdbf5ba9e3b6901f637ca410763d24a3
Sceglia2 <93980392+Sceglia2@users.noreply.github.com>
2023-01-21 09:09:44 +0100

    add icon in the pdf and margins for the text

--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/state/PDF.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/state/PDF.java
@@ -4,18 +4,25 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.ServiceRequest;
 
 import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 import it.unisa.diem.dhsa.group3.resources.DiagnosticReportResource;
+import it.unisa.diem.dhsa.group3.resources.ServiceRequestResource;
 
 
 public final class PDF {
@@ -32,38 +39,33 @@ public final class PDF {
 		metaData.put("patient id", subject);
 		Patient p = (Patient)ServerInteraction.getResource(Patient.class, subject);
 		int index = 0;
-		// it insert the first name in the field of the name
+		
 		for (HumanName humanName : p.getName()) {
 			if (humanName.getUse().equals(HumanName.NameUse.OFFICIAL)) {
+				metaData.put("patient name", "");
+				metaData.put("patient surname", "");
 				if (!humanName.getGiven().isEmpty()) {
 					index = humanName.getGiven().size() - 1;
 					metaData.put("patient name", humanName.getGiven().get(index).getValueNotNull()); // it insert the last name in the field of the names
 					metaData.put("patient surname", humanName.getFamily());
-				}else {
-					metaData.put("patient name", "");
-					metaData.put("patient surname", "");
 				}
-				// it insert the prefix in the corresponding field
+				
+				metaData.put("patient name prefix", "");
 				if (!humanName.getPrefix().isEmpty()) {
 					index = humanName.getPrefix().size() - 1;
 					metaData.put("patient name prefix", humanName.getPrefix().get(index).getValueNotNull());
-				}else {
-					metaData.put("patient name prefix", "");
 				}
-				// it insert the first name in the corresponding field
+				
+				metaData.put("patient name suffix", "");
 				if (!humanName.getSuffix().isEmpty()) {
 					index = humanName.getSuffix().size() - 1;
 					metaData.put("patient name suffix", humanName.getSuffix().get(index).getValueNotNull());
-				}else {
-					metaData.put("patient name suffix", "");
 				}
 			}
+			metaData.put("maiden", "");
 			if (humanName.getUse().equals(HumanName.NameUse.MAIDEN)) {
-				// it insert the last name in the field of the names
 				metaData.put("maiden", humanName.getFamily()); // only one maiden name for person
-			}else {
-				metaData.put("maiden", "");
-			}
+			}		
 		}
 		metaData.put("birth date", p.getBirthDate().toString());
 		
@@ -71,132 +73,127 @@ public final class PDF {
 		metaData.put("age", age.toString());
 		
 		metaData.put("encounter id", encounter);
-		//metaData.put("encounter class", encounter.getClass_().getDisplay());
-		//metaData.put("encounter description", encounter.getTypeFirstRep().getText());
 		
 		metaData.put("service request id", serviceReq);
+		ServiceRequest sr = (ServiceRequest)ServerInteraction.getResource(ServiceRequest.class,serviceReq);
+		ServiceRequestResource srr = new ServiceRequestResource(sr);
+		metaData.put("date", srr.getDate().toString());
 		metaData.put("imaging study id", imStudy);
-		//ImagingStudy im = (ImagingStudy)ServerInteraction.getGenericResource("imaging study",imStudy);
-		//metaData.put("imaging study modality", im.getSeriesFirstRep().getModality().getDisplay());
-		//metaData.put("body site", im.getSeriesFirstRep().getBodySite().getDisplay());
-		//metaData.put("date", im.getStarted().toString());
 		
 		metaData.put("comparison","None available");
 		metaData.put("technique", "Not available");
-		metaData.put("findings", "details");//drr.getDetails()
-		//dovrebbe essere presente in DiagnostiicReportResource
+		metaData.put("findings", srr.getDetails());
 		metaData.put("impression", drr.getConclusion());
-		
-		//metaData.put("requester", encounter.getParticipantFirstRep().getIndividualTarget().getId());
-		//metaData.put("provider", encounter.getServiceProviderTarget().getName());
 		
 		return metaData;
 		
 	}
 	
-	private static void writeNewLine(int offset, String line) {
+	private static void writeText(int offset, List<String> report) {
+		
 		try {
 			
-			content.newLineAtOffset(0, offset);
-			content.showText(line);
-			
+			PDFont pdfFont = PDType1Font.TIMES_ROMAN;
+		    float fontSize = 12;
+		    float leading = 1.5f * fontSize;
+		    System.out.println("leading " + leading);
+	
+		    PDRectangle mediabox = page.getMediaBox();
+		    float margin = 72;
+		    float width = mediabox.getWidth() - 2*margin;
+		    float startX = mediabox.getLowerLeftX() + margin;
+		    float startY = mediabox.getUpperRightY() - margin - 20;
+
+		    content.setFont(pdfFont, fontSize);
+		    content.beginText();
+		    content.newLineAtOffset(startX, startY);
+		    
+		    for(String text: report) {
+		    	List<String> lines = new ArrayList<String>();
+			    int lastSpace = -1;
+			    while (text.length() > 0){
+			        int spaceIndex = text.indexOf(' ', lastSpace + 1);
+			        if (spaceIndex < 0)
+			            spaceIndex = text.length();
+			        String subString = text.substring(0, spaceIndex);
+			        float size = fontSize * pdfFont.getStringWidth(subString) / 1000;
+			        //System.out.printf("'%s' - %f of %f\n", subString, size, width);
+			        if (size > width){
+			            if (lastSpace < 0)
+			                lastSpace = spaceIndex;
+			            subString = text.substring(0, lastSpace);
+			            lines.add(subString);
+			            text = text.substring(lastSpace).trim();
+			            //System.out.printf("'%s' is line\n", subString);
+			            lastSpace = -1;
+			        }
+			        else if (spaceIndex == text.length()){
+			            lines.add(text);
+			            //System.out.printf("'%s' is line\n", text);
+			            text = "";
+			        }
+			        else{
+			            lastSpace = spaceIndex;
+			        }
+			    }		    
+				//content.beginText();
+				
+			    for (String line: lines){
+			        content.showText(line);
+			        content.newLineAtOffset(0, -leading);
+			    }
+			    content.newLineAtOffset(0, offset);
+		    }
+		    
+		    //content.endText();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
+	}	    
 	
 	public static void createPDF(Map<String,String> metaData) throws IOException{
 		doc.addPage(page);
 		
+		//add icon to the file
 		content = new PDPageContentStream(doc, page);
+		String path = "src\\main\\java\\it\\unisa\\diem\\dhsa\\group3\\state\\icon64.png";
+		File f = new File(path);
+		PDImageXObject image = PDImageXObject.createFromFileByContent(f, doc);
+		content.drawImage(image, 75, 725);
+		
+		// add title
 		content.beginText();
 		content.setFont(PDType1Font.TIMES_BOLD, 16);
 		content.newLineAtOffset(200, 750);
 		content.showText("Hospital Information System - Diagnostic Report");
-		content.newLineAtOffset(-170, -70);
-		content.setFont(PDType1Font.TIMES_ROMAN, 14);
-		writeNewLine(0, "PATIENT: " + metaData.get("patient name prefix") + " " 
-									+ metaData.get("patient name") + " " 
-									+ metaData.get("patient name suffix") + " " 
-									+ metaData.get("maiden") + " " 
-									+ metaData.get("patient surname"));
-		writeNewLine(-30, "PATIENT ID: " + metaData.get("patient id"));
-		writeNewLine(-30, "DOB: " + metaData.get("birth date") + " - AGE: " + metaData.get("age"));
+		content.endText();
 		
-		writeNewLine(-40, "Diagnostic Report  n. " + metaData.get("imaging study id"));
-		writeNewLine(-30, "EXAM DATE: " + metaData.get("date"));
+		List<String> report = new ArrayList<>();
+		report.add("PATIENT: " + metaData.get("patient name prefix") + " " 
+				+ metaData.get("patient name") + " " 
+				+ metaData.get("patient name suffix") + " " 
+				+ metaData.get("maiden") + " " 
+				+ metaData.get("patient surname"));
+		report.add( "PATIENT ID: " + metaData.get("patient id"));
+		report.add("DOB: " + metaData.get("birth date") + " - AGE: " + metaData.get("age"));
+		report.add("Diagnostic Report  n. " + metaData.get("imaging study id"));
+		report.add( "Refer to Request  n. " + metaData.get("service request id"));
+		report.add("EXAM DATE: " + metaData.get("date"));
+		report.add("(ENCOUNTER ID) " + metaData.get("encounter id"));
+		report.add("Comparison: " + metaData.get("comparison"));
+		report.add("Technique: " + metaData.get("technique"));
+		report.add("Findings:" + metaData.get("findings"));
+		report.add("Conlusion:" + metaData.get("impression"));
 		
-		writeNewLine(-40, "Refer to Request  n. " + metaData.get("service request id"));
-		writeNewLine(-30, "EXAM DATE: " + metaData.get("date"));
+		writeText(-10,  report);
 		
-		//writeNewLine(-20, metaData.get("imaging study modality") + " - " + metaData.get("body site"));
-		
-		//writeNewLine(-20, "Indication: " + metaData.get("encounter class") + " - " + metaData.get("encounter description"));
-		writeNewLine(-30, "(ENCOUNTER ID) " + metaData.get("encounter id"));
-		
-		writeNewLine(-40, "Comparison: " + metaData.get("comparison"));
-		
-		writeNewLine(-30, "Technique: " + metaData.get("technique"));
-		
-		writeNewLine(-30, "Findings:");
-		for(String s: metaData.get("findings").split(" ")) {
-			writeNewLine(-20, s);
-		}
-		
-		writeNewLine(-20, "Conlusion:");
-		for(String s: metaData.get("impression").split(" ")) {
-			writeNewLine(-20, s);
-		}
-		
-		//writeNewLine(-30, "Doctor: " + metaData.get("requester"));
-		//writeNewLine(-10, "Structure: " + metaData.get("provider"));
-		
-		//String dicom = "a17d18afe1a71.2.840.99999999.23660322.844779366809.dcm";
-		//PDImageXObject image = PDImageXObject.createFromFile("C:\\Users\\annin\\Documents\\coherent-11-17-2022\\output\\dicom\\" + dicom, doc);
-		//content.drawImage(image, 0, -10);
 		content.endText();
 		content.close();
 		doc.save("Report" + metaData.get("service request id") + ".pdf");
 		doc.close();
 	}
 
-	
-	
-	public static void createPDF(String patient, String encounter, String conclusion, String details, String report_number, String request, String study) throws IOException {
-		doc.addPage(page);
-		
-			content = new PDPageContentStream(doc, page);
-			content.beginText();
-			content.setFont(PDType1Font.TIMES_BOLD, 16);
-			content.newLineAtOffset(200, 750);
-			content.showText("Hospital Information System");
-			content.newLineAtOffset(-170, -70);
-			content.setFont(PDType1Font.TIMES_ROMAN, 14);
-			content.showText("Patient n. " + patient + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Encounter  n. " + encounter + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Description: " + details + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Diagnosis " + conclusion + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Refer to Request n. " + request + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Study n.  " + study + " ");
-			content.newLineAtOffset(0, -70);
-			content.showText("Diagnostic Report  n." + report_number  + " ");
-			//String dicom = "a17d18afe1a71.2.840.99999999.23660322.844779366809.dcm";
-			//PDImageXObject image = PDImageXObject.createFromFile("C:\\Users\\annin\\Documents\\coherent-11-17-2022\\output\\dicom\\" + dicom, doc);
-			//content.drawImage(image, 0, -10);
-			content.endText();
-			content.close();
-			doc.save("Report" + request + ".pdf");
-			doc.close();
-			
-	}
-	
 	public static void  loadPDF(String report_number) throws IOException {
 		File file = new File("Report" + report_number + ".pdf");
 		Desktop desktop = Desktop.getDesktop();
