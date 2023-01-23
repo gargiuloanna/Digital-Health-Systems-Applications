3c93baf329a8e04e59a1b408bd80e008871fb7cd
Anna Gargiulo <a.gargiulo45@studenti.unisa.it>
2023-01-23 22:41:27 +0100

    added doc to CSV

--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/CSV/LoadCSV.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/CSV/LoadCSV.java
@@ -3,8 +3,18 @@ package it.unisa.diem.dhsa.group3.CSV;
 import it.unisa.diem.dhsa.group3.resources.*;
 import it.unisa.diem.dhsa.group3.state.Memory;
 
+/**
+ * The class allows to read a bunch of CSVs and sava them in the Memory map
+ *
+ */
 public final class LoadCSV {
-
+	
+	/**
+	 * The method get the FHIR class corresponding to the CSV read and inserts in the Memory map the corresponding class, and the resources
+	 * created reading the CSV.
+	 * @param filename  the file to read
+	 * @param resource	the string associated with the name of the resource
+	 */
 	public static void importing_resource(String filename, String resource) {
 		
 		resource = resource.toLowerCase();
@@ -15,6 +25,11 @@ public final class LoadCSV {
 		}
 	}
 
+	/**
+	 * The method maps the string corresponding to the name of the resource to the actual class of the resource.
+	 * @param resource the string name of the resource
+	 * @return the class of the resource
+	 */
 	public static Class<? extends BaseResource> get_class(String resource) {
 		switch (resource) {
 		case "patients":
--- HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/CSV/ReadCSV.java
+++ HIS_Project/src/main/java/it/unisa/diem/dhsa/group3/CSV/ReadCSV.java
@@ -11,8 +11,19 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import it.unisa.diem.dhsa.group3.resources.BaseResource;
 import it.unisa.diem.dhsa.group3.state.Memory;
 
+/**
+ * The class provides methods to read a single CSV
+ *
+ */
 public class ReadCSV {
 
+	/**
+	 * The method reads a CSV file and saves into the Memory map the class of the resource read and a map of all the resources read from
+	 * the CSV.
+	 * @param specificClass the class of the resources contained in the CSV
+	 * @param path the path of the file
+	 * @return the map containing the (id of the resource, resource) for each entry of the csv
+	 */
 	public  static Map<String, Resource> readCSV(Class<? extends BaseResource> specificClass, String path) {
 
 		Iterator<BaseResource> iter;
